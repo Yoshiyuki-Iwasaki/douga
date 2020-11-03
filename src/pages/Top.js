0dea947e0d092237ef4e5cd7a516ad26c8c9b998
Yoshiyuki Iwasaki <yoshiyuki_iwasaki@commude.co.jp>
2020-11-04 01:37:53 +0900

    [add] toppage動画表示

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -2,6 +2,9 @@ import React, { useEffect, useContext } from 'react';
 import Layout from '../components/Layout/Layout.js';
 import { fetchPopularDate } from '../apis/index';
 import { Store } from '../store/index';
+import VideoGrid from '../components/VideoGrid/VideoGrid.js';
+import VideoGridItem from '../components/VideoGridItem/VideoGridItem.js';
+
 
 const Top = () => {
   const { globalState, setGlobalState } = useContext(Store);
@@ -13,7 +16,20 @@ const Top = () => {
   }, [])
   return (
     <Layout>
-      top page
+      <VideoGrid>
+        {
+          globalState.popular && globalState.popular.map((popular) => { 
+            return (
+              <VideoGridItem
+                id={popular.id}
+                key={popular.id}
+                src={popular.snippet.thumbnails.standard.url}
+                title={popular.snippet.title}
+              />
+            )
+          })
+        }
+      </VideoGrid>
     </Layout>
   )
 }
