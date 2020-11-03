b648844487dfeaa892cba9ed61377090ea6d5f9d
Yoshiyuki Iwasaki <yoshiyuki_iwasaki@commude.co.jp>
2020-11-04 00:40:18 +0900

    [add] store追加

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import Layout from '../components/Layout/Layout.js';
 import { fetchPopularDate } from '../apis/index';
+import { Store } from '../store/index';
 
 const Top = () => {
-
+  const { globalState, setGlobalState } = useContext(Store);
   useEffect(() => {
-    fetchPopularDate().then((res) => { 
+    fetchPopularDate().then((res) => {
       console.log('data', res);
+      setGlobalState({type: 'SET_POPULAR', payload: { popular: res.data.items }})
     })
   }, [])
   return (
